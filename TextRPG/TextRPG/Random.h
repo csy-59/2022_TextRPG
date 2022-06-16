04f8541cc8728c40d16c5910013dd7ea5fef0811
csy-59 <csy010509@naver.com>
2022-06-16 16:05:37 +0900

    [Style] 주석 추가_220616

--- TextRPG/TextRPG/Random.h
+++ TextRPG/TextRPG/Random.h
@@ -10,9 +10,20 @@ public:
 	Random& operator=(const Random&) = delete;
 	~Random() = default;
 
+	/// <summary>
+	/// Random을 초기화한다.
+	/// </summary>
 	static void Init();
+
+	/// <summary>
+	/// [minNum, maxNum]까지의 랜덤 수를 생성한다.
+	/// </summary>
+	/// <param name="minNum">최소 수</param>
+	/// <param name="maxNum">최대 수</param>
+	/// <returns>생성한 난수</returns>
 	static int GetRandomNumberFromRange(int minNum, int maxNum);
 };
+
 void Random::Init()
 {
 	srand(time(NULL));
