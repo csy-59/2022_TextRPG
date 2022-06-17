a8537675436e38c51b958b26c07cbae49677d648
csy-59 <csy010509@naver.com>
2022-06-17 19:47:49 +0900

    [Fix] UI 변경_220617

--- TextRPG/TextRPG/HowToScene.cpp
+++ TextRPG/TextRPG/HowToScene.cpp
@@ -5,13 +5,13 @@ using namespace std;
 void HowToScene::Render() const
 {
 	cout << "[How to Play]\n";
-	cout << "# 선택지 이동: 숫자 입력 후 enter\n";
+	cout << "※ 선택지 이동: 숫자 입력 후 enter\n";
 	cout << "\n";
-	cout << "# ♥: 체력. 0이 되면 사망합니다.\n";
-	cout << "# S: 스트레스. 스트레스가 높으면 더 높은 공격을 받습니다.\n";
-	cout << "	 스트레스가 50에 도달하면 사망합니다.\n";
+	cout << "※ ♥: 체력. 0이 되면 사망합니다.\n";
+	cout << "※ S: 스트레스.스트레스가 높으면 더 높은 공격을 받습니다.\n";
+	cout << "	 스트레스가 70에 도달하면 사망합니다.\n";
 	cout << "\n";
-	cout << "게임 클리어 조건: 레벨이 30이 되면 게임 클리어!\n";
+	cout << "※게임 클리어 조건: 레벨이 30이 되면 게임 클리어!\n\n";
 
 	printOptions();
 }
