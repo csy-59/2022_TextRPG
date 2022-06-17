c8a9b85eed8666be1735c39e100b9aeb5a228000
csy-59 <csy010509@naver.com>
2022-06-17 19:46:06 +0900

    [Feat] 실수 랜덤 생성 함수 완성_220617

--- TextRPG/TextRPG/Random.h
+++ TextRPG/TextRPG/Random.h
@@ -22,6 +22,15 @@ public:
 	/// <param name="maxNum">최대 수</param>
 	/// <returns>생성한 난수</returns>
 	static int GetRandomNumberFromRange(int minNum, int maxNum);
+
+	/// <summary>
+	/// [minNum, maxNum]까지의 랜덤 실수를 생성한다.
+	/// </summary>
+	/// <param name="minNum">최소 수</param>
+	/// <param name="maxNum">최대 수</param>
+	/// <returns>생성한 랜덤 실수</returns>
+	static float GetRandomFloatNumberFromRange(float minNum, float maxNum);
+
 };
 
 void Random::Init()
@@ -34,6 +43,15 @@ int Random::GetRandomNumberFromRange(int minNum, int maxNum)
 	float normalized = (float) rand() / RAND_MAX;
 	int range = maxNum - minNum + 1;
 	int result = minNum + (int)(range * normalized);
+
+	return result;
+}
+
+float Random::GetRandomFloatNumberFromRange(float minNum, float maxNum)
+{
+	float normalized = (float)rand() / RAND_MAX;
+	float range = maxNum - minNum + 1;
+	float result = minNum + range * normalized;
 
 	return result;
 }
