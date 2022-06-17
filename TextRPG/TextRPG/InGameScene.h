a60d122b93498007b41c620a199a8d5927b3d78c
csy-59 <csy010509@naver.com>
2022-06-17 20:34:34 +0900

    [Feat] InGameScene 기본 메소드 추가, SETW_NUM 추가_220617

--- TextRPG/TextRPG/InGameScene.h
+++ TextRPG/TextRPG/InGameScene.h
@@ -4,9 +4,16 @@
 class InGameScene :public Scene
 {
 public:
+	InGameScene() = default;
+	InGameScene(const InGameScene& other) = default;
+	InGameScene& operator=(const InGameScene& other) = default;
+	~InGameScene() = default;
+
 	virtual void Render();
 private:
 	virtual void PrintHeaderInfo() const = 0;
 	virtual void PrintMainMessage() const = 0;
 	virtual void PrintFooterInfo() const = 0;
+public:
+	static const int SETW_NUM = 23;
 };
